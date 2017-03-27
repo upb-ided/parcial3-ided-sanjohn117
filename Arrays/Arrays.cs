efa003ac06a7c9d45087f74feef1112d7de03a7f
Héctor C. Alzate <halzate93@gmail.com>
2017-03-26 21:16:49 -0500

    Added initial tests

---
+++ Arrays/Arrays.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Arrays
+{
+	public class Arrays
+	{
+		public int[] Sort (int[] numbers)
+		{
+			return numbers;
+		}
+
+		public int Average(int[] numbers)
+		{
+			int sum = 0;
+			for (int i = 0; i < numbers.Length; i++)
+				sum += numbers[i];
+			return sum / numbers.Length;
+		}
+	}
+}
---
+++ Arrays/ArraysTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+namespace Arrays
+{
+	[TestFixture()]
+	public class ArraysTest
+	{
+		private Arrays toTest;
+
+		[SetUp]
+		public void Prepare()
+		{
+			toTest = new Arrays ();
+		}
+
+		[Test()]
+		public void AveragePositiveNumbers ()
+		{
+			int[] numbers = new int[] { 1, 2, 3 };
+			int average = toTest.Average (numbers);
+			Assert.AreEqual (2, average);
+		}
+
+		[Test()]
+		public void SortReversedArray ()
+		{
+			int[] numbers = new int[] { 3, 2, 1 };
+			int[] sorted  = toTest.Sort (numbers);
+			Assert.AreEqual(new int[] { 1, 2, 3}, sorted);
+		}
+	}
+}
