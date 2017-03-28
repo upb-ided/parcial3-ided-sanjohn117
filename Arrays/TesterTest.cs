3fc49fbcb547231e7e8aa5e02cfbd2708656fc0e
DARK_ETERNAL <tomailsandstuff@gmail.com>
2017-03-27 22:42:43 -0500

    Homework code in progress
    Added homework skeleton. Test cases in progress

---
+++ Arrays/TesterTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace Arrays
+{
+    [TestFixture()]
+    internal class TesterTest
+    {
+        private const string paramOne = "holamUndo";
+
+        private Tester toTest;
+
+        [SetUp]
+        public void Prepare()
+        {
+            toTest = new Tester();
+        }
+
+        //Prueba del punto 1
+        [TestCase("anitalavalatina", 'a', 6)]
+        [TestCase("eraseunavezunablancapaloma", 'Z', 0)]
+        [TestCase("holAmundo", 'a', 0)]
+        public void CountCharacterA(string inputString, char inputChar, int compareResultWith)
+        {
+            Assert.AreEqual(compareResultWith, toTest.CountCharInString(inputString, inputChar));
+        }
+    }
+}
