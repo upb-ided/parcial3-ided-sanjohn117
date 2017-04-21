ace16fb57479d4e07957234dc3fe5325ba7f1667
DARK_ETERNAL <tomailsandstuff@gmail.com>
2017-04-20 22:12:41 -0500

    Added exam base code

---
+++ Arrays/MatrixOperator.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Parcial3_Base
+{
+    public static class MatrixExtensions
+    {
+        public static void PrintMatrixValues(this int[,] matrix)
+        {
+            //Cómo se usa? matrix.PrintMatrixValues();
+            //Siendo 'matrix' el nombre de la matriz
+
+            if (matrix != null)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        Console.WriteLine(string.Format("[{0},{1}] = {2}", i, j, matrix[i, j]));
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Matrix is null");
+            }
+        }
+
+        public static void PrintDimensions(this int[,] inputMatrix)
+        {
+            Console.WriteLine(string.Format("Matrix has dimension {0}x{1}", inputMatrix.GetLength(0), inputMatrix.GetLength(1)));
+        }
+    }
+
+    internal class MatrixOperator
+    {
+        /// <summary>
+        /// Suma las matrices parámetro.
+        /// Valor del punto: 1.0 / 5.0
+        /// </summary>
+        /// <param name="matrixA"></param>
+        /// <param name="matrixB"></param>
+        /// <returns>La matriz suma</returns>
+        public int[,] AddMatrix(int[,] matrixA, int[,] matrixB)
+        {
+            int[,] result = null;
+
+            if (result != null)
+            {
+                result.PrintMatrixValues();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Multiplica una matriz por un escalar
+        /// Valor del punto: 1.0/ 5.0
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="scalar"></param>
+        /// <returns>La matriz producto por escalar</returns>
+        public int[,] MultiplyMatrixByScalar(int[,] matrix, int scalar)
+        {
+            int[,] result = null;
+
+            if (result != null)
+            {
+                result.PrintMatrixValues();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Multiplica dos matrices parámetro
+        /// Valor del punto: 2.0 / 5.0
+        /// </summary>
+        /// <param name="matrixA"></param>
+        /// <param name="matrixB"></param>
+        /// <returns>La matriz producto</returns>
+        public int[,] MultiplyMatrices(int[,] matrixA, int[,] matrixB)
+        {
+            int[,] result = null;
+
+            if (result != null)
+            {
+                result.PrintMatrixValues();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Mezcla los elementos de dos matrices parámetro
+        /// Valor del punto: 1.0 / 5.0
+        /// </summary>
+        /// <param name="matrixA"></param>
+        /// <param name="matrixB"></param>
+        /// <returns>La matriz mezcla</returns>
+        public int[,] MixMatrix(int[,] matrixA, int[,] matrixB)
+        {
+            /*
+             * La operación ficticia 'mezcla' será definida así.
+             * Sean dos matrices 'matrixA' y 'matrixB' de igual dimensión
+             * La mezcla de esas matrices dará como resultado una matriz 'result'
+             * Los elementos de 'result' seguirán la siguiente regla:
+             * a. Un elemento de la diagonal será el producto de los elementos de 'matrixA' y 'matrixB' en esa posición
+             * b. Cualquier elemento debajo de la diagonal será el elemento de 'matrixA' en esa posición
+             * c. Cualquier elemento encima de la diagonal será el elemento de 'matrixB' en esa posición
+             */
+
+            int[,] result = null;
+
+            if (result != null)
+            {
+                result.PrintMatrixValues();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Arrays/MatrixOperatorTest.cs
@@ -0,0 +1,134 @@
+﻿using NUnit.Framework;
+
+namespace Parcial3_Base
+{
+    [TestFixture()]
+    internal class MatrixOperatorTest
+    {
+        public static object[] MatrixAddCases =
+        {
+            new object[]
+            {
+                new int[,] { { 1, 0}, { 0, 1} },
+                new int[,] { { 0, 1}, { 1, 0} },
+                new int[,] { { 1, 1}, { 1, 1} }
+            },
+            new object[]
+            {
+                new int[,] { { 1, 2}, { -1, 0} },
+                new int[,] { { -2, 9}, { 3, 5} },
+                new int[,] { { -1, 11}, { 2, 5} }
+            },
+            new object[]
+            {
+                new int[,] { { 6, 12}, { -14, 0} },
+                new int[,] { { -3, 9}, { 2, -16} },
+                new int[,] { { 3, 21}, { -12, -16} }
+            },
+            new object[]
+            {
+                new int[,] { { 3, 0, 3}, { 0, 6, -6 }, { -3, 6, -3} },
+                new int[,] { { -1, -3,-2 }, { -1, 9, 5 }, { -3, -1, 1} },
+                new int[,] { { 2, -3, 1}, { -1, 15, -1 }, { -6, 5, -2} },
+            },
+        };
+
+        public static object[] MatrixProductCases =
+        {
+            new object[] {
+                new int[,] { { 2 }, { -1 } },
+                new int[,] { { 3, 5} },
+                new int[,] { { 6, 10}, { -3, -5} }
+            },
+            new object[] {
+                new int[,] { { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, 9 } },
+                new int[,] { { 1, -1, 2 }, { 2, -1, 2 }, { 3, -3, 0 } },
+                new int[,] { { 30, -26, 10}, { 36, -31, 14 }, { 42, -36, 18 } }
+            },
+            new object[] {
+                new int[,] { { -3, 0, 2 }, { -1, 0, 1}, { 2, 5, -2 } },
+                new int[,] { { 3, 1, 5 }, { 0, -2, 6}, { 3, -3, 7 } },
+                new int[,] { { -3, -9, -1 }, { 0, -4, 2 }, { 0, -2, 26 } }
+            },
+            new object[] {
+                new int[,] { { 1, 2 }, { -1, 0 }, { -3, -1 } },
+                new int[,] { { 2, 0, 1 }, { -5, 2, 3 } },
+                new int[,] { { -8, 4, 7 }, { -2, 0, -1 }, { -1, -2, -6 } }
+            },
+
+            new object[] {
+                new int[,] { { 1, 2 }, { -1, 0 }, { -3, -1 } },
+                new int[,] { { 2, 0, 1 }},
+                null
+            },
+        };
+
+        public static object[] MatrixMixCases =
+        {
+             new object[] {
+                new int[,] { { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } },
+                new int[,] { { 4, 5, 6 }, { 4, 5, 6 }, { 4, 5, 6 } },
+                new int[,] { { 4, 5, 6 }, { 1, 10, 6 }, { 1, 2, 18 } }
+            },
+
+             new object[] {
+                new int[,] { { 10, 5, 8, 4 }, { 12, 9, 4, 16 }, { 3, 8, 9, 20 }, { 4, 15, 23, 1} },
+                new int[,] { { 1, 0, 0, 0}, { 0, 1, 0, 0 }, { 0, 0, 1, 0}, { 0, 0, 0, 1} },
+                new int[,] { { 10, 0, 0, 0 }, { 12, 9, 0, 0 }, { 3, 8, 9, 0 }, { 4, 15, 23, 1} }
+            },
+        };
+
+        private static object[] ProductByScalarCases = 
+        {
+             new object[] {
+                new int[,] { { 10, 5, 8, 4 }, { 12, 9, 4, 16 }, { 3, 8, 9, 20 }, { 4, 15, 23, 1} },
+                6,
+                new int[,] { { 60, 30, 48, 24 }, { 72, 54, 24, 96 }, { 18, 48, 54, 120 }, { 24, 90, 138, 6} },
+            },
+             new object[] {
+                new int[,] { { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } },
+                1500,
+                new int[,] { { 1500, 3000, 4500 }, { 1500, 3000, 4500 }, { 1500, 3000, 4500 } },
+             },
+              new object[] {
+                new int[,] { { 1, 2 }, { -1, 0 }, { -3, -1 } },
+                2,
+                new int[,] { { 2, 4 }, { -2, 0 }, { -6, -2 } },
+            },
+
+        };
+        
+
+        private MatrixOperator operatorObject;
+
+        [SetUp]
+        public void Prepare()
+        {
+            operatorObject = new MatrixOperator();
+        }
+
+        [Test, TestCaseSource("MatrixAddCases")]
+        public void TestAddMatrices(int[,] matrixA, int[,] matrixB, int[,] compareResultWith)
+        {
+            Assert.AreEqual(compareResultWith, operatorObject.AddMatrix(matrixA, matrixB));
+        }
+
+        [Test, TestCaseSource("MatrixProductCases")]
+        public void TestMultiplyMatrices(int[,] matrixA, int[,] matrixB, int[,] compareResultWith)
+        {
+            Assert.AreEqual(compareResultWith, operatorObject.MultiplyMatrices(matrixA, matrixB));
+        }
+
+        [Test, TestCaseSource("MatrixMixCases")]
+        public void TestMixMatrices(int[,] matrixA, int[,] matrixB, int[,] compareResultWith)
+        {
+            Assert.AreEqual(compareResultWith, operatorObject.MixMatrix(matrixA, matrixB));
+        }
+
+        [Test, TestCaseSource("ProductByScalarCases")]
+        public void TestProductByScalar(int[,] matrix, int scalar, int[,] compareResultWith)
+        {
+            Assert.AreEqual(compareResultWith, operatorObject.MultiplyMatrixByScalar(matrix, scalar));
+        }
+    }
+}
